615530515b8e0fa24cb459628c349a43329d508c
Serkan OZCAN <serkan@Serkan-MacBook-Pro.local>
2017-07-16 22:14:02 +0300

    -

--- example.php
+++ example.php
@@ -1,10 +1,12 @@
 <?php
-
 require './vendor/autoload.php';
 
 try {
+    // use true for debug
+    // $resetIPResult = new \IOSResetIP\IP(true);
 
     $resetIPResult = new \IOSResetIP\IP();
+    var_dump($resetIPResult->getIP());
 
 } catch (\Exception $e) {
     echo $e->getMessage();
--- src/IP.php
+++ src/IP.php
@@ -3,32 +3,87 @@ namespace IOSResetIP;
 
 use IOSResetIP\Exceptions;
 
+/**
+ * IP Address resetter for jailbroken IOS Phones on cellular data.
+ * Turns off cellular data and turns it on again.
+ * 
+ * @author SRKNZCN <serkanozcan@gmail.coms>
+ */
 class IP {
-
+    protected $debug = null;
     protected $delayForRetry = 1;
+    protected $activatorCommand = "activator";
     protected $switchOffCellularDataCommand = "activator send switch-off.com.a3tweaks.switch.cellular-data";
     protected $switchOnCellularDataCommand = "activator send switch-on.com.a3tweaks.switch.cellular-data";
+    protected $checkNewIPUrl = "http://ip2json.info/?key=remoteAddr";
 
-    public function __construct() {
-        $turnOffCellular = shell_exec($this->switchOffCellularDataCommand);
-        if (!$turnOffCellular) {
+    public $IP;
+
+    public function __construct($debug = false) {
+        $this->debug = $debug;
+
+        $isActivator = shell_exec("which " . $this->activatorCommand);
+        if (!$isActivator) {
             throw new Exceptions\ActivatorNotFoundException();
             return false;
         }
+        $this->switchOffCellularData();
         sleep($this->delayForRetry);
-        shell_exec($this->switchOnCellularDataCommand);
+        $this->switchOnCellularData();
 
-        // // check new IP Addr
+        // check new IP Addr
+        return $this->checkForNewIPAddress();
+    }
+
+    /**
+     * Checks ip query service until new connection detects
+     * 
+     * @return void
+     */
+    public function checkForNewIPAddress() {
         $newIP = FALSE;
-        
-        // while ( !$newIP ) {
-        //     $newIP = doRequest('http://ripequery.com/?key?remoteAddr', $ua, $ur);
-        //     if ($newIP) {
-        //         echo ">>> NEW IP ADDR : " . $newIP . "\n";
-        //         break;
-        //     } else
-        //         sleep(1);
-        // }
+        while ( !$newIP ) {
+            $newIPData = @file_get_contents($this->checkNewIPUrl);
+            if ($newIPData) {
+                $json = json_decode($newIPData);
+                $this->IP = $json->remoteAddr;
+                if ($this->debug) {
+                    echo ">>> NEW IP ADDR : " . $this->IP . "\n";
+                }
+                return $json->remoteAddr;
+            } else {
+                if ($this->debug) {
+                    echo ".";
+                }
+                sleep($this->delayForRetry);
+            }
+        }
+    }
 
+    /**
+     * Turns off cellular data
+     * 
+     * @return void
+     */
+    public function switchOffCellularData() {
+        shell_exec($this->switchOffCellularDataCommand);
+    }
+
+    /**
+     * Turns on cellular data
+     * 
+     * @return void
+     */
+    public function switchOnCellularData() {
+        shell_exec($this->switchOnCellularDataCommand);
+    }
+
+    /**
+     * Returns new IP address
+     * 
+     * @return string
+     */
+    public function getIP() {
+        return $this->IP;
     }
 }
